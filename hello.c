e9b03ecd81b98e02108ed769fa1dda06c2ebfcba
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-06-25 10:26:58 +0530

    sample commit

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+    // this is my first program
+    /*This is a comment */
+
+    printf("this is my first program\n");
     printf("hello\n");
-    return 0;
+     return 0;
 }
