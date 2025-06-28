aae4e5067bce4c1322618b24d202f18cde2fc1a3
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-06-28 12:56:06 +0530

    math functions

---
+++ math_function.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+
+{
+
+    //int x = 2;
+    //x= sqrt(x); // Calculate the square root of x
+    //printf("The square root of 9 is: %d\n", x); 
+
+    // x = pow(x, 2); // Calculate x raised to the power of 3
+    // printf("The value of x raised to the power of 3 is: %d\n", x);
+
+
+    //round function round(x) 
+
+    int x = -3;
+    int y;
+    y = abs(x); // Calculate the absolute value of x
+    printf("The absolute value of -3 is: %d\n", y);
+
+
+    return 0;
+}
