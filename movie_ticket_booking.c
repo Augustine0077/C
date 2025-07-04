14649ccdfc53c244396bc4bff02e457468a40bc5
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-04 22:21:34 +0530

    movie ticket boocking updated

--- movie_ticket_booking.c
+++ movie_ticket_booking.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 #include<stdbool.h>
 int main(){
-    float price =10;
-    bool is_stdent;
-    bool is_Senior;
+    float price = 10;
+    bool is_student = false; // 10%
+    bool is_senior = false;  // 20%
 
-   if(is_stdent){
-    printf("you get a 10% discount");
-    price *=0.9;
-   }
-   if(is_Senior){
-    printf("you get a 20%% discount");
-    price *=0.8;
-   }
-   
+    // Only apply discounts if the user is a student or senior
+    if (is_senior) {
+        if (is_student) {
+            printf("You are a student, you get a discount of 10%%\n");
+            printf("You are a senior, you get a discount of 20%%\n");
+            price *= 0.7;
+        } else {
+            printf("You are a senior, you get a discount of 20%%\n");
+            price *= 0.8;
+        }
+    } else if (is_student) {
+        printf("You are a student, you get a discount of 10%%\n");
+        price *= 0.9;
+    } else {
+        printf("No discount applied.\n");
+    }
+
+    printf("The price of a ticket is %.2f\n", price);
     return 0;
 }
