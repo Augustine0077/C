4ffbb51d09991b26bfaecbc70759042b4c10c13d
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-07 23:50:16 +0530

    new commit

--- temprature.c
+++ temprature.c
@@ -3,10 +3,10 @@ int main(){
     int temprature  =30;
 
     if((temprature > 0) && (temprature < 30)){
-        printf("The temprature is good");
+        printf("The temprature is bad");
     }
     else{
-        printf("The temprature is bad");
+    printf("The temprature is good");
     }
 
     return 0;
