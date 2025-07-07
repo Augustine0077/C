786637f9d9f6fcafdc561b0dc3efe2ba6b18b4a7
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-08 00:00:48 +0530

    new commit

---
+++ functins.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void Happy_Birthday(){
+    printf("Happy Birthday to you\n");
+    printf("Happy Birthday to you\n");
+    printf("Happy Birthday dear friend\n");
+    printf("Happy Birthday to you\n");
+}
+
+int main() {
+    Happy_Birthday();
+    return 0;
+}
