c0b21102d577f1447b8674ddffed35e32fb513f8
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-08 14:21:52 +0530

    squre of numbers using functions

--- functins.c
+++ functins.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-void Happy_Birthday(){
+#include<string.h>
+void Happy_Birthday(char name[],int age) {
     printf("Happy Birthday to you\n");
-    printf("Happy Birthday to you\n");
+    printf("Happy Birthday to %i\n",age);
     printf("Happy Birthday dear friend\n");
-    printf("Happy Birthday to you\n");
+    printf("Happy Birthday to %s\n", name);
 }
 
 int main() {
-    Happy_Birthday();
+    char name[20]="Augustine";
+    int age=12;
+    Happy_Birthday(name, age);
+    Happy_Birthday(name, age);
+    Happy_Birthday(name, age);
+  
     return 0;
 }
---
+++ squre.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ 
+int square (int num ){
+    int result = num * num;
+    return result;
+}
+int main() {
+ int x=square(2);
+ int y= square(4);
+ int z=square(5);
+    printf("The square of 2 is %d\n", x);
+    printf("The square of 4 is %d\n", y);
+    printf("The square of 5 is %d\n", z);
+    return 0;
+}
